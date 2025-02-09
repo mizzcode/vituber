00eed46b0d5426d6b644a09353fc6b7a50f0408c
Misbahudin <misbahudin.id@gmail.com>
2025-02-09 19:32:38 +0700

    Add initial project files including HTML, CSS, JavaScript, and assets for ViTuber

---
+++ main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const darkModeToggle = document.getElementById('darkmode-toggle');
+  const darkModeIcon = darkModeToggle.querySelector('img');
+  const darkModeEnabled = localStorage.getItem('darkMode') === 'enabled';
+  const menuToggle = document.getElementById('menu-toggle');
+  const navUl = document.querySelector('.nav ul');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      navUl.style.top = '6rem';
+    } else {
+      navUl.style.top = '6rem';
+    }
+  });
+
+  if (darkModeEnabled) {
+    document.body.classList.add('dark-mode');
+    darkModeIcon.src = '/assets/images/sun-svgrepo-com.svg';
+  }
+
+  darkModeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const darkModeIsActive = document.body.classList.contains('dark-mode');
+
+    if (darkModeIsActive) {
+      darkModeIcon.src = '/assets/images/sun-svgrepo-com.svg';
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      darkModeIcon.src = '/assets/images/moon-stars-svgrepo-com.svg';
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  });
+
+  menuToggle.addEventListener('click', function() {
+    navUl.classList.toggle('show');
+    const menuIcon = menuToggle.querySelector('i');
+    if (navUl.classList.contains('show')) {
+      menuIcon.classList.remove('fa-bars');
+      menuIcon.classList.add('fa-times');
+    } else {
+      menuIcon.classList.remove('fa-times');
+      menuIcon.classList.add('fa-bars');
+    }
+  });
+});
